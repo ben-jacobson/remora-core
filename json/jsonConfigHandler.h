b46730daed1b42a4e159e38021183a75fc400141
Ben <ben_jacobson@live.com>
2025-08-11 20:33:52 +1000

    Added include guard as platformio configurable switch for loading JSON from SD or FLASH. Added readConfigFromFlash, as well as CRC checks + implementation for Ethernet uploads. Added to updateThreadFreq a quick check to see if the threads were configured via config.txt and to read out default values if not set.

--- json/jsonConfigHandler.h
+++ json/jsonConfigHandler.h
@@ -1,4 +1,3 @@
-
 #ifndef JSON_CONFIG_HANDLER_H
 #define JSON_CONFIG_HANDLER_H
 
@@ -6,7 +5,22 @@
 #include <ArduinoJson.h>
 #include "fatfs.h"
 
+#ifdef ETH_CTRL
+#include "remora-hal/hal_utils.h"
+#endif
+
 class Remora; //forward declaration
+
+constexpr uint16_t metadata_len = 512; // bytes
+constexpr uint16_t metadata_padding_len = metadata_len - (sizeof(uint32_t) * 3);
+
+typedef struct __attribute__((packed))
+{
+  uint32_t crc32;   		// crc32 of JSON
+  uint32_t length;			// length in words for CRC calculation
+  uint32_t jsonLength;  	// length in of JSON config in bytes
+  uint8_t padding[metadata_padding_len];		// ensure struct size = metadata_len in bytes, as this is how TFTP is loading in packets. 
+} json_metadata_t;
 
 class JsonConfigHandler {
 private:
@@ -18,13 +32,21 @@ private:
 	JsonObject thread;
 	//bool configError;
 	uint8_t loadConfiguration();
-	uint8_t readFileContents();
+	uint8_t readConfigFromSD();
+	uint8_t readConfigFromFlash();	
 	uint8_t parseJson();
 
 public:
+	static volatile bool new_flash_json;
+
 	JsonConfigHandler(Remora* _remora);
 	void updateThreadFreq();
 	JsonArray getModules();
-	JsonObject getModuleConfig(const char* threadName, const char* moduleType);
+	JsonObject getModuleConfig(const char* threadName, const char* moduleType);	
+
+	int8_t json_check_length_and_CRC(void);
+	#ifdef ETH_CTRL
+	void store_json_in_flash(void);
+	#endif
 };
 #endif
