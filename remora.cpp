114db0534f8298e8277662d9930902362bf919a0
Scott <scottalford75@gmail.com>
2025-09-14 08:46:00 +1000

    Updated license

--- remora.cpp
+++ remora.cpp
@@ -1,3 +1,22 @@
+/*
+    Remora is a free, opensource LinuxCNC component and Programmable Realtime
+	Unit (PRU) firmware to implement a LinuxCNC based CNC controller.
+	Copyright (C) 2025 Scott Alford (scotta)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "remora.h"
 #include "../irqHandlers.h"
 #include "interrupt/interrupt.h"
